18066c377500c14ce1a8af39a858633dfed04d66
David Su <js202@terminator8.cs.rice.edu>
2024-07-18 14:30:18 -0500

    Clarify Unit test for K rope, mn, scale
    Before, since fused_rope_and_quant modifies K in place, it was
    unclear how it affects the reference test case.
    
    Now I moved the reference test case to the top, and used copies
    of K to make the reference, making sure that no Q,K,V is modified
    before the Action on fused_rope_and_quant.
    
    Currently, K's rope and min are the same as reference. k_scale has
    some difference, rendering k_quant different from reference.
    
    BUG: K_scale is not the same as reference. max diff around 0.001953

--- quant/test_rope.py
+++ quant/test_rope.py
@@ -1,8 +1,9 @@
 import torch
 import numpy as np
 import random
-from rope_embedding import fused_rope_and_quant, _fused_rope_embedding, rope_ref
+from rope_embedding import fused_rope_and_quant, _fused_rope_embedding, rope_ref, fast_rope_ref
 from new_pack import triton_pack_along_last_dim, triton_quantize_and_pack_along_last_dim, quant_and_pack_kcache
+from torch.profiler import profile, ProfilerActivity 
 
 if __name__ == '__main__':
     # position_ids.shape: [1, 744]
@@ -11,7 +12,8 @@ if __name__ == '__main__':
     torch.manual_seed(0)
     np.random.seed(0)
     random.seed(0)
-    BS, NH, SL, HD = 1, 32, 8192, 128
+    BS, NH, SL, HD = 1, 8, 8192, 64
+    # BS, NH, SL, HD = 1, 32, 8192, 128
     GS = 32
     BITS = 2
 
@@ -24,21 +26,41 @@ if __name__ == '__main__':
     V = torch.randn(BS, SL, NH, HD).half().cuda().transpose(1, 2)
     cos, sin = rotary_emb(Q, seq_len=SL)
 
-    # Test 
-    Q_our, K_our, v_mn, v_scale, k_mn, k_scale = fused_rope_and_quant(Q, K, V, cos, sin, position_ids,BITS, BITS, GS)
-    torch.cuda.synchronize()
-
-    # Assert
-    K_input = K.transpose(1,2).view(BS, SL, NH*HD)
-    K_rope_quant_ref, k_scale_ref, k_mn_ref = triton_quantize_and_pack_along_last_dim(K.permute(0,1,3,2).contiguous(), GS, BITS)
-    breakpoint()
+    # Reference: (MAKE SURE NO REFERENCE MODIFIES Q, K, V IN PLACE)
+    K_rope_ref = fast_rope_ref(K.clone(), cos, sin, position_ids)
+    # NOTE: reference quantization is applied AFTER ROPE has been applied
+    K_quant_ref, k_scale_ref, k_mn_ref = triton_quantize_and_pack_along_last_dim(K_rope_ref.permute(0,1,3,2).contiguous(), GS, BITS)
     k_mn_ref = k_mn_ref.transpose(2,3).contiguous() # BS, NH, NG, HD
+    k_scale_ref = k_scale_ref.transpose(2,3).contiguous() # BS, NH, NG, HD
+    
+    # Act 
+    Q_rope, K_rope, v_mn, v_scale, k_mn, k_scale = fused_rope_and_quant(Q, K.clone(), V, cos, sin, position_ids,BITS, BITS, GS)
+    torch.cuda.synchronize()
+    torch.set_printoptions(edgeitems=4, linewidth=180, sci_mode=False, precision=6)
 
     _, _, v_mn_ref = triton_quantize_and_pack_along_last_dim(V, GS, BITS)
-    diff_v = ((v_mn_ref - v_mn) / v_mn_ref).abs()
+    diff_v = (v_mn_ref - v_mn)
     print("diff_V_mn: ", diff_v.max())
 
-    # diff_k = ((k_mn_ref - k_mn) / k_mn_ref).abs()
-    diff_k = k_mn_ref - k_mn
-    print(diff_k)
-    print(f"diff_K: {diff_k.max()}")
+    # Assert K rope
+    diff_K_rope = K_rope_ref - K_rope
+    print("diff_K_rope: ", diff_K_rope.max())
+
+    # Assert K mn
+    diff_k_mn = k_mn_ref - k_mn
+    print(f"diff_K_mn: {diff_k_mn.max()}")
+
+    # Assert K scale
+    diff_k_scale = (k_scale_ref - k_scale)
+    print(f"diff_K_scale: {diff_k_scale}")
+    print(f"diff_K_scale: {diff_k_scale.max()}")
+    breakpoint()
+    # Assert K quant
+    k_rope_quant = triton_pack_along_last_dim(K_rope.transpose(2,3).contiguous(),
+                                              k_mn.transpose(2,3).contiguous(),
+                                              k_scale_ref.transpose(2,3).contiguous(),
+                                              GS, 
+                                              BITS)
+    diff_k_quant = (K_quant_ref - k_rope_quant)
+    print(f"diff_K_quant: {diff_k_quant.max()}")
+    breakpoint()    
